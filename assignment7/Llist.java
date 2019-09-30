a3dbf2365d2657248d75ddca50e6fe0fc8b4dce7
Rishi <42106072+rishivar@users.noreply.github.com>
2019-10-01 01:17:33 +0530

    final edit

--- assignment7/Llist.java
+++ assignment7/Llist.java
@@ -38,6 +38,7 @@ public class Llist {
         ele = in.nextInt();
         llist.addLast(ele);
         System.out.println("Modified List is "+ llist);
+        System.out.println();
         
         System.out.println("Enter Element to insert at position: ");
         ele = in.nextInt();
@@ -45,11 +46,14 @@ public class Llist {
         pos = in.nextInt();
         llist.add(pos, ele);
         System.out.println("Modified List is "+ llist);
-
+        System.out.println();
+        
         System.out.println("Removing elements from both sides!");
         llist.removeFirst(); 
         llist.removeLast();
         System.out.println("Modified List is "+ llist);
+        System.out.println();
+
         
         System.out.println("Enter element to remove: ");
         ele = in.nextInt();
@@ -59,39 +63,44 @@ public class Llist {
             System.out.println("No Element Found!");
         
         System.out.println("List After Deletion: "+ llist);
-        
+        System.out.println();
 
         LinkedList<Integer> k = new LinkedList<Integer>();
         Collections.copy(llist,k);
-
         
         Collections.sort(llist);
         System.out.println("Sorted List: "+ llist);
-       
+        System.out.println();
+        
         System.out.println("Enter element to find position: ");
         ele = in.nextInt();
         pos = llist.indexOf(ele);
         System.out.println("Position of "+ ele + " in list : "+ pos);
-        
-        LinkedList<Integer> tmp = new LinkedList<Integer>();
-        tmp = llist;
+        System.out.println();
+                
         System.out.println("Displaying list in forward order "+ llist);
         System.out.println("\nDisplaying list in reverse order");
+        
+        System.out.println("\nUn Algorithm Manner");
+        ListIterator<Integer> litr = llist.listIterator();
+        while(litr.hasNext())
+            litr.next();
+        while(litr.hasPrevious())
+        {
+            System.out.print(litr.previous() + " ");
+        }
+        
+        
         System.out.println("\nAlgorithm Manner");
         Collections.reverse(llist);
         System.out.println(llist);
-        
-        System.out.println("\nUn Algorithm Manner");
-        ListIterator<Integer> litr = tmp.listIterator(tmp.size());
-        while(litr.hasPrevious())
-        {
-            System.out.print(litr.previous());
-        }
+
         
         int total = 0, avg;
         for(i = 0; i<llist.size(); i++)
         total = total+llist.get(i);
         avg = total / llist.size();
+        
         System.out.println("\n\nThe Total is: "+ total);
         System.out.println("The Average IS:" + avg);
         
@@ -101,9 +110,6 @@ public class Llist {
         result.addAll(k);
         result.addAll(newll);
         System.out.println("Newly Created List is: "+ result);
-
-    
-    
     }
     
 }
